2a214e85620bd6595d2f436971fe191880187029
Ashi-commits <67394408+Ashi-commits@users.noreply.github.com>
2021-07-31 19:42:53 -0400

    Update ClientPipe.sh

--- ClientPipe.sh
+++ ClientPipe.sh
@@ -1,8 +1,10 @@
-fifo=/tmp/log_server_pipe 
-if [[ ! -p $fifo ]] 
-   then echo "Sorry, log server not running" >&2 
-        exit 1 
-fi 
-printf "Please enter a comment: " 
-read log_record 
-echo "$log_record" > $fifo
+#!/bin/bash
+
+fifo=/tmp/log_server_pipe
+if [[ ! -p $fifo ]]
+ then echo "Sorry, log server not running" >&2
+      exit 1
+fi
+printf "Please enter a comment: "
+read log_record
+echo "$log_record" >$fifo
